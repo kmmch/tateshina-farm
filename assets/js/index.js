a71d807e9fa327e8adf443ebe48f8d422c70b8a3
kmmch <kmmch.tech@gmail.com>
2023-09-15 22:54:10 +0900

    不要コメント削除

--- assets/js/index.js
+++ assets/js/index.js
@@ -56,60 +56,46 @@ HAMBURGER_MENU_LINK_IDS.forEach((id) => {
 // スライダー
 // --------------------------------------------
 const swiper = new Swiper(".swiper", {
-  /*
-  // ループ
-  loop: true,
-
-  // 一度に表示する枚数
-  slidesPerView: 4,
-
-  // アクティブなスライドを中央にする
-  centeredSlides: true,
-
-  // ループの時間
-  speed: 6000,
-
-  // スワイプ無効
-  allowTouchMove: false,
-
-  //自動再生
-  autoplay: {
-
-    // 途切れなくループ
-    delay: 0,
-
-    // 矢印をクリックしても自動再生を止めない
-    disableOnInteraction: false,
-  },
-  */
+    loop: true,
+    autoplay: {
+        delay: 0,
+    },
+    speed: 3000,
+    centeredSlides : true,
+    slidesPerView: 1.5, // スマホでは1枚を中央に、.5で見切れ具合を調整
+    spaceBetween:10,
+    breakpoints: {
+        768: {
+            slidesPerView: 3.5,// PCでは3枚を中央に、.5で見切れ具合を調整
+            spaceBetween:20,
+        }
+    },
+    loopAdditionalSlides: 1,
+});
 
 
-  loop: true,
-  autoplay: {
-    delay: 0,
-  },
-  speed: 3000,
-  centeredSlides : true,
-  slidesPerView: 1.5, // スマホでは1枚を中央に、.5で見切れ具合を調整
-  spaceBetween:10,
-  breakpoints: {
-    768: {
-      slidesPerView: 3.5,// PCでは3枚を中央に、.5で見切れ具合を調整
-      spaceBetween:20,
-    }
-  },
-  loopAdditionalSlides: 1,
 
-
-  // ページネーション
-  // pagination: {
-  //   el: ".swiper-pagination",
-  //   clickable: true,
-  // },
-
-  // 前後の矢印
-  // navigation: {
-  //   nextEl: ".swiper-button-next",
-  //   prevEl: ".swiper-button-prev",
-  // },
+// --------------------------------------------
+// お問い合わせ送信ボタン押下時の自画面遷移
+// --------------------------------------------
+$(document).ready(function () {
+        $('#contactForm').submit(function (event) {
+        var formData = $('#contactForm').serialize();
+        $.ajax({
+            url: "https://docs.google.com/forms/hogehoge",
+            data: formData,
+            type: "POST",
+            dataType: "xml",
+            statusCode: {
+                0: function () {
+                    $(".end-message").slideDown();
+                    $(".submit-btn").fadeOut();
+                },
+                200: function () {
+                    $(".false-message").slideDown();
+                }
+            }
+        });
+        event.preventDefault();
+    });
 });
