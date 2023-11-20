8daa54387b374d2ac6633c1db23bfaee99a76d32
kmmch <kmmch.tech@gmail.com>
2023-11-20 10:59:59 +0900

    アコーディオン調整

--- assets/js/index.js
+++ assets/js/index.js
@@ -71,6 +71,10 @@ const swiper = new Swiper('.swiper', {
     spaceBetween:10,
     breakpoints: {
         768: {
+            slidesPerView: 3.5,
+            spaceBetween:20,
+        },
+        1030: {
             slidesPerView: 5.5,// PCでは3枚を中央に、.5で見切れ具合を調整
             spaceBetween:20,
         }
